55be798f2392182fd0ceeedca6f4801c1542885f
Georg Augusto Schlegel <georgaugusto@gmail.com>
2020-10-08 20:14:58 -0300

    Removes unnecessary files

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,11 @@ import { BrowserRouter } from 'react-router-dom';
 
 import GlobalStyle from './styles/global';
 
-import AppProvider from './hooks';
-
 import Routes from './routes';
 
 const App: React.FC = () => (
   <BrowserRouter>
-    <AppProvider>
       <Routes />
-    </AppProvider>
-
     <GlobalStyle />
   </BrowserRouter>
 );
